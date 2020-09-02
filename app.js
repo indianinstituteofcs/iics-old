8af2664338012c677133c61a69293abd79eb66d4
Isha Chirimar <isha.chirimar@gmail.com>
2020-09-02 16:58:26 -0400

    modifying login page

--- app.js
+++ app.js
@@ -30,8 +30,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/login', loginRouter);
 app.use('/class', classRouter);
+app.use('/login', loginRouter);
 app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
 app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js'));
 
