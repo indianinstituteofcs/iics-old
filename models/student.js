70c040d947c8bf2cdc523a156a96cda6275872d4
Isha Chirimar <isha.chirimar@gmail.com>
2020-09-02 17:38:14 -0400

    schema for students and parents

---
+++ models/student.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const StudentSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  parent: {
+    type: String,
+    required: true
+  }
+});
+const Student = mongoose.model('Student', StudentSchema);
+
+module.exports = Student;
